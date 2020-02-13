e31776933866b3877d0805cbb56c58fd75469492
IVAN LUIS DE OLIVEIRA JUNIOR <ivan.lojunior@senacsp.edu.br>
2020-02-13 14:45:01 -0300

    first commit

---
+++ apptest.js
@@ -0,0 +1,44 @@
+
+// HelloWorld = function () {
+//     return "Hello Jovem ";
+// }
+
+
+function Buenosdias(){
+    console.log("Bom dia!")
+} Buenosdias();
+
+console.log ("------");
+
+var nome = ["João", "Rafael", "Ricardo"];
+
+function Bomdianome(nome){
+    console.log("Bom dia, " + nome + "!")
+
+
+}   Bomdianome(nome[2]);
+
+
+
+console.log ("------ arrow function -----");
+
+Bomdia = () => "Bom dia! "
+
+const msg = Bomdia();
+
+console.log(msg);
+
+console.log ("------");
+
+var name = ["Rachel", "Ivan", "Luan"]
+
+Bomdianom = (name) => `Bom dia! ${[]}`
+    
+
+ Bomdianom([2]);
+
+
+
+
+
+
